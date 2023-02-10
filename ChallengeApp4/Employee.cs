abcf1b5c4abca396f2c1073c618ab3c1bab06b48
bravebarry <wojciechowskimarcink@gmail.com>
2023-02-10 01:20:45 +0100

    Zadanie 6

---
+++ ChallengeApp4/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace ChallengeApp4
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public List<int> Ratings { get; set; }
+
+        public Employee(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Ratings = new List<int>();
+        }
+    }
+}
--- ChallengeApp4/Program.cs
+++ ChallengeApp4/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ChallengeApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Users = new List<Employee>();
+
+            Users.Add(new Employee("Stefan", "Batory", 45));
+            Users.Add(new Employee("Marek", "Adamski", 29));
+            Users.Add(new Employee("Sebastian", "Nowak", 38));
+
+            Users[0].Ratings.AddRange(new int[] { 10, 9, 8, 7, 6 });
+            Users[1].Ratings.AddRange(new int[] { 5, 5, 5, 5, 5 });
+            Users[2].Ratings.AddRange(new int[] { 10, 10, 10, 9, 8 });
+
+            Employee highestRatedEmployee = Users[0];
+            int highestAverageRating = (int)Users[0].Ratings.Average();
+
+            foreach (var employee in Users)
+            {
+                if (employee.Ratings.Average() > highestAverageRating)
+                {
+                    highestRatedEmployee = employee;
+                    highestAverageRating = (int)employee.Ratings.Average();
+                }
+            }
+
+            Console.WriteLine("Pracownik z największą średnią ocen:");
+            Console.WriteLine("Imię: " + highestRatedEmployee.FirstName);
+            Console.WriteLine("Nazwisko: " + highestRatedEmployee.LastName);
+            Console.WriteLine("Wiek: " + highestRatedEmployee.Age);
+            Console.WriteLine("Oceny: " + string.Join(", ", highestRatedEmployee.Ratings));
+        }
+    }
+}
